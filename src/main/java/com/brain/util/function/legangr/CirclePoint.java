24f462247ec7b58fb0b04bbe86952eb6acddd15e
epopov <36furious@gmail.com>
2021-06-12 22:02:46 +0300

    Добавлено кеширование сферических функций

---
+++ src/main/java/com/brain/util/function/legangr/CirclePoint.java
@@ -0,0 +1,16 @@
+package com.brain.util.function.legangr;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode
+public class CirclePoint {
+
+    private int L;
+    private int m;
+    private double teta;
+    private double fi;
+}
--- src/main/java/com/brain/util/function/legangr/NormMultiplier.java
+++ src/main/java/com/brain/util/function/legangr/NormMultiplier.java
@@ -1,11 +1,22 @@
 package com.brain.util.function.legangr;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class NormMultiplier {
+
+    private static Map<CirclePoint, Double> circleFunMap = new LinkedHashMap<>();
 
     /**
      * Описывает распространение электрических потенциалов по сфере мозга
      */
     public static double Y(int L, int m, double teta, double fi) throws RuntimeException {
+        CirclePoint circlePoint = new CirclePoint(L, m, teta, fi);
+
+        if (circleFunMap.containsKey(circlePoint)) {
+            return circleFunMap.get(circlePoint);
+        }
+
         double result = calculeteAttachedNormMultiplierLegandra(L, m);
 
         if (m >= 0){
@@ -13,6 +24,9 @@ public class NormMultiplier {
         } else {
             result *= PolynomLejandra.mergePolinomLegandra(Math.cos(teta), L, Math.abs(m)) * Math.sin(Math.abs(m) * fi);
         }
+
+        circleFunMap.put(circlePoint, result);
+
         return result;
     }
 
@@ -21,7 +35,7 @@ public class NormMultiplier {
      */
     public static double calculeteAttachedNormMultiplierLegandra(int L, int m){
         int ksi = m == 0 ? 1 : 0;
-        return Math.sqrt((2 * L + 1) / ( 2 * Math.PI * (1 + ksi)) * calculateFactDelLegandra(L, m));
+        return Math.sqrt((2. * L + 1.) / ( 2. * Math.PI * (1. + ksi)) * calculateFactDelLegandra(L, m));
     }
 
     /**
@@ -32,7 +46,7 @@ public class NormMultiplier {
             throw new RuntimeException("NaN");
         }
 
-        double result = 1;
+        double result = 1.;
         for (int i = L - Math.abs(m); i < L + Math.abs(m); i++){
             result /= i == 0 ? 1 : i;
         }
